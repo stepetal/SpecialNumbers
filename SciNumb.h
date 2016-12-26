128ef53c853151e7bfa84a500d9d52aad3ed7a79
stepetal <stepetal94@rambler.ru>
2016-12-26 14:24:22 +0500

    Create SciNumb.h

---
+++ SciNumb.h
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <regex>
+#include <memory>
+
+enum InOutTypes{ Console, File };
+
+class InputType{
+	std::vector<std::string> info;
+	std::regex number_format;
+public:
+	virtual void GetInfo(std::string input_name)=0;
+	std::vector<std::string> ReturnInput(){ return info; }
+	void SetInfo(std::vector<std::string> v){ info = v; }
+protected:
+	void SetNumberFormat(std::regex reg_expr){ number_format = reg_expr; }
+	
+};
+
+class InputConsole : public InputType{
+public:
+	void GetInfo(std::string input_name);
+};
+
+class InputFile :public InputType{
+public:
+	void GetInfo(std::string input_name);
+};
+
+class OutputType{
+	std::vector<std::string> info_out;
+public:
+	virtual void WriteInfo(std::string output_name) = 0;
+	void SetOutInfo(std::shared_ptr<InputType> i_type_info){ info_out = i_type_info->ReturnInput(); }
+protected:
+	
+	std::vector<std::string> GetOutInfo(){ return info_out; } 
+};
+
+class OutputFile :public OutputType{
+	void WriteInfo(std::string output_name);
+};
+
+class OutputConsole :public OutputType{
+	void WriteInfo(std::string output_name);
+};
+
+
+
+
+class SciNumb{
+	std::shared_ptr<InputType> i_type_number;
+public:
+	void SetSciNumb(std::shared_ptr<InputType> v){ i_type_number = v; }
+	std::shared_ptr<InputType> GetSciNumb(){ return i_type_number; }
+};
+
+InputType* GetInputType(InOutTypes i_o_type);
+OutputType* GetOutputType(InOutTypes i_o_type);
